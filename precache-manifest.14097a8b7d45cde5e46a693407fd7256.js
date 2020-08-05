b55efa9d8ef36a5d0416a0fe51293cc2d6dec503
john <johnrkennedy54@gmail.com>
2020-08-04 22:37:00 -0400

    Updates

---
+++ precache-manifest.14097a8b7d45cde5e46a693407fd7256.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ec0b17ebb7a91380b354f4ffc1f9e3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7d1bb3ca66951bf67332",
+    "url": "/portfolio/static/css/2.f7c64eb8.chunk.css"
+  },
+  {
+    "revision": "7eae5df8cb55a5d1f3b1",
+    "url": "/portfolio/static/css/main.bd5336e1.chunk.css"
+  },
+  {
+    "revision": "7d1bb3ca66951bf67332",
+    "url": "/portfolio/static/js/2.1925491f.chunk.js"
+  },
+  {
+    "revision": "7eae5df8cb55a5d1f3b1",
+    "url": "/portfolio/static/js/main.bbfd3b46.chunk.js"
+  },
+  {
+    "revision": "d7d4bba4e78eb725924d",
+    "url": "/portfolio/static/js/runtime~main.33b23d1f.js"
+  },
+  {
+    "revision": "7c33af2f1a5b73e5d6884ea1846a054d",
+    "url": "/portfolio/static/media/boost-your-post-for-increasing-sales.7c33af2f.md"
+  },
+  {
+    "revision": "9f553f26846bbd2553a9ed2e3a604ed6",
+    "url": "/portfolio/static/media/difference-between-gatsbyjs-and-nextjs.9f553f26.md"
+  },
+  {
+    "revision": "8cc16894642414291f3700ac5396c1ae",
+    "url": "/portfolio/static/media/future-of-javascript.8cc16894.md"
+  },
+  {
+    "revision": "94fbd3842288f7042fb36a2b025fb7a5",
+    "url": "/portfolio/static/media/how-to-choose-javascript-framework-for-project.94fbd384.md"
+  },
+  {
+    "revision": "6566a29212d29ce3455d349471c4c4c3",
+    "url": "/portfolio/static/media/how-to-get-10k-instagram-followers.6566a292.md"
+  },
+  {
+    "revision": "368cc11c0fc1f5868a2461a538b6dd81",
+    "url": "/portfolio/static/media/installing-nodejs-on-your-device.368cc11c.md"
+  },
+  {
+    "revision": "c7fe48e63067433331f84ec94e2e49fc",
+    "url": "/portfolio/static/media/markdown-html-supported-blog.c7fe48e6.md"
+  },
+  {
+    "revision": "d4881e738d4008ebb0ad0ed9e6587da5",
+    "url": "/portfolio/static/media/popular-javascript-library-in-2020.d4881e73.md"
+  },
+  {
+    "revision": "d490be272abdf01ca26f90685cec9b99",
+    "url": "/portfolio/static/media/programmers-must-read-books.d490be27.md"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "c0c0a9c9cc01fca9adedff7998f6ac05",
+    "url": "/portfolio/static/media/think-out-of-the-box.c0c0a9c9.md"
+  },
+  {
+    "revision": "ea70d882cb4362f1ca2c8db077b78794",
+    "url": "/portfolio/static/media/time-to-use-latest-technology-for-creating-a-website.ea70d882.md"
+  },
+  {
+    "revision": "8e1efcf25b01edbf3d60df3f09646750",
+    "url": "/portfolio/static/media/trending-designs-in-2020.8e1efcf2.md"
+  },
+  {
+    "revision": "c7973cd5f0da590119aa22cd3147a84f",
+    "url": "/portfolio/static/media/uiux-design-starter-with-adobe-xd.c7973cd5.md"
+  },
+  {
+    "revision": "a1c7c37f05e57483b9ecb3edbdf5e5a3",
+    "url": "/portfolio/static/media/web-automation-with-python-language.a1c7c37f.md"
+  },
+  {
+    "revision": "40686a4e31c7336491e3abce057010eb",
+    "url": "/portfolio/static/media/what-nodejs-can-do.40686a4e.md"
+  }
+]);
